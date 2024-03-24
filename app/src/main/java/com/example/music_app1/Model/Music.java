a9d507d68704340167d98b339725fe9289a99367
Toan <trandinhtoan2610@gmail.com>
2024-03-24 14:14:30 +0700

    play music

---
+++ app/src/main/java/com/example/music_app1/Model/Music.java
@@ -0,0 +1,50 @@
+package com.example.music_app1.Model;
+
+public class Music {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    private int id;
+    private String name;
+    private String artist;
+    private String image;
+    private String link;
+
+}
